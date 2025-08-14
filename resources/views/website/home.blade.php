9e14dfbcb3bea787c70e807ea123113d25e42b74
Marcos Borges <110690829+marcosborg@users.noreply.github.com>
2025-08-14 15:24:54 +0100

    Update

--- resources/views/website/home.blade.php
+++ resources/views/website/home.blade.php
@@ -96,37 +96,38 @@
                     d="m2.165 15.803.02-.004c1.83-.363 2.948-.842 3.468-1.105A9.06 9.06 0 0 0 8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6a10.437 10.437 0 0 1-.524 2.318l-.003.011a10.722 10.722 0 0 1-.244.637c-.079.186.074.394.273.362a21.673 21.673 0 0 0 .693-.125zm.8-3.108a1 1 0 0 0-.287-.801C1.618 10.83 1 9.468 1 8c0-3.192 3.004-6 7-6s7 2.808 7 6c0 3.193-3.004 6-7 6a8.06 8.06 0 0 1-2.088-.272 1 1 0 0 0-.711.074c-.387.196-1.24.57-2.634.893a10.97 10.97 0 0 0 .398-2" />
             </svg> Chat online
         </div>
-        <div class="card-body" id="chat-container"></div>
+        <div>
+<div class="card-body" id="chat-container"></div>
         <div class="card-footer">
             <textarea class="form-control" id="message-textarea" placeholder="Escreva uma mensagem"></textarea>
         </div>
-    </div>
-
-    <!-- Modal Condições de Utilização -->
-    <div class="modal fade" id="termsModal" tabindex="-1" aria-labelledby="termsModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg modal-dialog-centered">
-        <div class="modal-content">
-        <div class="modal-header bg-warning">
-            <h5 class="modal-title" id="termsModalLabel">Condições de Utilização do Chat</h5>
         </div>
-        <div class="modal-body">
-            <p>
-            Este chat é controlado por uma Inteligência Artificial (IA) e destina-se a auxiliar com informações gerais.
-            Apesar de todos os esforços para garantir a precisão, as respostas podem conter erros ou imprecisões.
-            </p>
-            <p>
-            Ao utilizar este chat, concorda que as informações fornecidas não substituem aconselhamento profissional e
-            que podem ser sujeitas a falhas técnicas ou interpretações incorretas.
-            </p>
-            <p>
-            Se concordar, clique em "Aceito" para continuar.
-            </p>
+        <!-- Modal Condições de Utilização -->
+        <div class="modal fade" id="termsModal" tabindex="-1" aria-labelledby="termsModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg modal-dialog-centered">
+                <div class="modal-content">
+                <div class="modal-header bg-warning">
+                    <h5 class="modal-title" id="termsModalLabel">Condições de Utilização do Chat</h5>
+                </div>
+                <div class="modal-body">
+                    <p>
+                    Este chat é controlado por uma Inteligência Artificial (IA) e destina-se a auxiliar com informações gerais.
+                    Apesar de todos os esforços para garantir a precisão, as respostas podem conter erros ou imprecisões.
+                    </p>
+                    <p>
+                    Ao utilizar este chat, concorda que as informações fornecidas não substituem aconselhamento profissional e
+                    que podem ser sujeitas a falhas técnicas ou interpretações incorretas.
+                    </p>
+                    <p>
+                    Se concordar, clique em "Aceito" para continuar.
+                    </p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" id="acceptTerms" class="btn btn-primary">Aceito</button>
+                </div>
+                </div>
+            </div>
         </div>
-        <div class="modal-footer">
-            <button type="button" id="acceptTerms" class="btn btn-primary">Aceito</button>
-        </div>
-        </div>
-    </div>
     </div>
 
 
